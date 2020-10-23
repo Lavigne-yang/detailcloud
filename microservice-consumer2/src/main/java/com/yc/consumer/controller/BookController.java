8075dd8a1a06ee24fe858cb728b2a0c35b919662
Lavigne-yang <2034549297@qq.com>
2020-10-23 19:45:41 +0800

    记录负载日志显示服务提供方信息

--- microservice-consumer2/src/main/java/com/yc/consumer/controller/BookController.java
+++ microservice-consumer2/src/main/java/com/yc/consumer/controller/BookController.java
@@ -3,6 +3,8 @@ package com.yc.consumer.controller;
 
 import com.yc.consumer.domian.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
@@ -23,6 +26,10 @@ public class BookController {
     //直接访问eureka中的服务名即可,这样 ribbon会拉取到服务实例列表，再利用负载均衡算法获取一个服务.
     private final static String RESTURI="http://MICROSERVICE-PROVIDER/";
     //                                   http://ip:port/
+
+
+    @Resource
+    private LoadBalancerClient LoadBalancerClient;  //服务提供方法信息
 
 
     @Autowired
@@ -37,7 +44,17 @@ public class BookController {
         //return restTemplate.getForObject(URL+id,Book.class);
         //   HttpEntity(    MultiValueMap)   ,    HttpHeaders: MultiValueMap
         //   返回: ResponseEntity
+
+        //获取服务提供方信息
+        ServiceInstance serviceInstance=this.LoadBalancerClient.choose("MICROSERVICE-PROVIDER");
+        String ip=serviceInstance.getHost();
+        int port=serviceInstance.getPort();
+        System.out.println("" +
+                "【**  服务提供实例信息  **】 host="+ip +
+                ", port="+port+
+                ",serviceid="+serviceInstance.getServiceId());
         return restTemplate.exchange( RESTURI+"book/"+id, HttpMethod.GET, new HttpEntity<Object>(httpHeaders), Book.class ).getBody();
+        //return restTemplate.exchange("http://"+ip+":"+port+"/"+"book/"+id,HttpMethod.GET,new HttpEntity<Object>(httpHeaders), Book.class ).getBody();
     }
 
     @GetMapping("/findAll")
